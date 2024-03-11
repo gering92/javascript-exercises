0acc5ff796bc534faa953fe17c6c83dc4133784a
gering92 <geringeorge92@gmail.com>
2024-03-11 10:37:45 -0700

    Finished the sumAll exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(numStart, numEnd) {
+    let sum = 0;
+    if (numStart < 0 || numEnd < 0) {
+        return "ERROR";
+    } else if (typeof numStart !== "number" || typeof numEnd !== "number") {
+        return "ERROR"
+    } else {
+        let smallerNumber = numStart;
+        let largerNumber = numEnd
+        if (numEnd < numStart) {
+            smallerNumber = numEnd;
+            largerNumber = numStart;
+        }
+        for (let i = smallerNumber; i <= largerNumber; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
