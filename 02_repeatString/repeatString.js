01efb2d5e21a5b8b39878e7611609e412eecc486
gering92 <geringeorge92@gmail.com>
2024-03-11 09:38:54 -0700

    Finished repeatString and reverseString exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, numRepeat) {
+    let result = "";
+    if (numRepeat >= 0) {
+        for (i = 0; i < numRepeat; i++) {
+            result += phrase;
+        }
+    } else {
+        return "ERROR"
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let temp = "";
+    let pointer = "";
+    pointer = phrase.length - 1;
+    if (phrase.length = 0) {
+        return phrase;
+    } else {
+        for (let i = pointer; i >= 0; i--) {
+            temp += phrase[i];
+        }
+    }
+    return temp;
 };
 
 // Do not edit below this line
