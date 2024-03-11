d7f6c91c40d2c4355500b9fca7b28097584284c5
gering92 <geringeorge92@gmail.com>
2024-03-11 10:15:38 -0700

    Finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...moreNums) {
+    let newArray;
+    if (moreNums.length > 0) {
+        newArray = array.filter(item => !moreNums.includes(item))
+        return newArray;
+    } else {
+        return newArray;
+    }
+    
 };
 
 // Do not edit below this line
